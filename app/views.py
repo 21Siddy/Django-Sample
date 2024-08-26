1f6c5870afb253a7326148d4dd6663f930107464
Siddharth Hemnani <73987718+21Siddy@users.noreply.github.com>
2024-08-26 22:48:42 +0200

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from . import limesurvey
+from . import models
 # Create your views here.
 def index(request):
     return HttpResponse("Hello")
@@ -8,6 +9,12 @@ def index(request):
 
 def suggestion(request):
     new_suggestion = str(limesurvey.get_latest_suggestion())
-    print(new_suggestion)
-    context = {'suggestions': new_suggestion}
+    new_item, created = models.Item.objects.get_or_create(item_name=new_suggestion)
+    item_set = models.Item.objects.all().distinct()
+    item_list = [
+        item['item_name'] 
+        for item in item_set.values('item_name') 
+        if item['item_name'] and item['item_name'] != 'None'
+    ]
+    context = {'suggestions': item_list}
     return JsonResponse(context)
