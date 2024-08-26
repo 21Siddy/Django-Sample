c7bf9fb4fea3649d8078143c30881514b78832dc
Siddharth Hemnani <73987718+21Siddy@users.noreply.github.com>
2024-08-26 14:24:59 +0200

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from . import limesurvey
 # Create your views here.
 def index(request):
     return HttpResponse("Hello")
+
+
+def suggestion(request):
+    new_suggestion = str(limesurvey.get_latest_suggestion())
+    print(new_suggestion)
+    context = {'suggestions': new_suggestion}
+    return JsonResponse(context)
