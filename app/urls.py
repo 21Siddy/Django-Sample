93f640d5c9d8a899afa3d6808ddf8282fa0a2f30
Siddharth Hemnani <73987718+21Siddy@users.noreply.github.com>
2024-08-27 11:51:35 +0200

    Add files via upload

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('suggestion/', views.suggestion, name='suggestion')
+    path('suggestion/', views.suggestion, name='suggestion'),
+    path('ratings/', views.post_suggestion_ratings, name='ratings')
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from . import limesurvey
 from . import models
 # Create your views here.
@@ -9,7 +11,7 @@ def index(request):
 
 def suggestion(request):
     new_suggestion = str(limesurvey.get_latest_suggestion())
-    new_item, created = models.Suggestion.objects.get_or_create(suggestion=new_suggestion)
+    new_item, created = models.Suggestion.objects.get_or_create(item_name=new_suggestion)
     item_set = models.Suggestion.objects.all().distinct()
     item_list = [
         item['suggestion'] 
@@ -18,3 +20,22 @@ def suggestion(request):
     ]
     context = {'suggestions': item_list}
     return JsonResponse(context)
+
+suggestion_rating_dict = {} #to store the solution and rating as key value pairs
+@csrf_exempt
+def post_suggestion_ratings(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            selected_value = data.get("solution")
+            selected_rating = data.get("rating")
+            suggestion_rating_dict[f"{selected_value}"] = selected_rating
+            response_data = {
+                "message": "Rating received successfully!",
+            }
+            print(suggestion_rating_dict) #temporarily stored in a dictionary - should be saved to Db
+            return JsonResponse(response_data, status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+        
+    return JsonResponse({"error": "Invalid request method"}, status=405)
