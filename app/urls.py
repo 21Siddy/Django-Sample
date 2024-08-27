dc4bc337f33ed247c736c0f4fe3b0f1b7e9f7927
Siddharth Hemnani <73987718+21Siddy@users.noreply.github.com>
2024-08-27 12:14:04 +0200

    Add files via upload

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('suggestion/', views.suggestion, name='suggestion'),
-    path('ratings/', views.post_suggestion_ratings, name='ratings')
+    path('', views.index, name="index"),
+    path('suggestion/', views.suggestion, name="suggestion"),
+    path('ratings/', views.post_suggestion_ratings, name="ratings")
 ]
